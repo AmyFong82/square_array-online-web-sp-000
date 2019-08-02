8a0ff4958e8e9568f3ed3b653d7f1bd231ae7f87
Amy Fong <amyhfong@gmail.com>
2019-08-02 19:09:53 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,12 @@
-numbers = [1,2,3]
+# numbers = [1,2,3]
 def square_array(array)
   new_array = []
-  array.each do |number| 
+  array.each do |number|
     squared_num = number ** number
     new_array << squared_num
   end
 end
 
-square_array(numbers)
-
-numbers = [1,2,3]
+# square_array(numbers)
+#
+# numbers = [1,2,3]
